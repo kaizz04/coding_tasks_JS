4be886ee8d49b050da6b619fde02b11ff16c9eb5
kaizz04 <kaveesh@marketpushapps.com>
2024-09-06 17:26:25 +0530

    Fibonacci task

--- main.js
+++ main.js
@@ -100,3 +100,20 @@ function factorial(n1){
 }
 // console.log(factorial(5));
 
+function getFibonacci(){
+    const cache = {}; 
+
+  function fib(n) {
+    
+    if (n <= 1) return n; 
+
+    if (cache[n]) return cache[n]; 
+    cache[n] = fib(n - 1) + fib(n - 2); 
+    return cache[n]; 
+  }
+
+  return fib;
+
+}
+const fib = getFibonacci();
+console.log(fib(10));
